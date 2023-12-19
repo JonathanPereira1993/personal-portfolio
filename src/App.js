a0639d7f6347d604c53b3ffa38f30a92b209a03a
Jonathan Pereira <96317160+JonathanPereira1993@users.noreply.github.com>
2023-12-19 09:03:28 +0000

    feat: Installed the Tailwind and tested it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <>Hello There</>;
+  return (
+    <>
+      <div className="p-4 bg-slate-400">
+        <h1 className="font-bold text-4xl">Hello There</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
